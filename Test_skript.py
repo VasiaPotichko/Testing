6583e55bbdae8fe0d7668311977cf31a55a3a93c
Vasia_Potichko <ia.tasher228@gmail.com>
2025-03-18 00:15:30 +0100

    Или сюда? Or in this

--- Test_skript.py
+++ Test_skript.py
@@ -1,10 +1,10 @@
 from random import randint
 
 while True:
-    username = input("Пожалуйста введите свое имя")
-    randigit = randint(101) # случайное число
+    username = input("Пожалуйста введите свое имя -  ")
+    randigit = randint(1, 101) # случайное число
 
-    print(f"Привет{username}! твое случайное число: {randigit}"
+    print(f"Привет {username}! твое случайное число: {randigit}")
 
     if username == "пожалуйста хватит":
         break 
